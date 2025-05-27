7d675a085ca30cf2671a3938d778085936fd3563
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-05-27 14:54:24 +0200

    Formatting

--- utxo-accumulator-api/src/ZkFold/Cardano/UtxoAccumulator/Precompute.hs
+++ utxo-accumulator-api/src/ZkFold/Cardano/UtxoAccumulator/Precompute.hs
@@ -2,18 +2,13 @@
 
 module ZkFold.Cardano.UtxoAccumulator.Precompute where
 
+import Data.Aeson (decode)
+import Data.ByteString (fromStrict)
+import Data.FileEmbed (embedFileRelative)
+import Data.Maybe (fromJust)
+import ZkFold.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1_Point)
 import ZkFold.Cardano.UtxoAccumulator.Constants (N)
 import ZkFold.Data.Vector (Vector, unsafeToVector)
-import ZkFold.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1_Point)
-import Data.FileEmbed (embedFileRelative)
-import Data.Aeson (decode)
-import Data.Maybe (fromJust)
-import Data.ByteString (fromStrict)
--- import ZkFold.Symbolic.Examples.UtxoAccumulator (
---   accumulationGroupElements,
---   distributionGroupElements,
---  )
--- import qualified ZkFold.Symbolic.Examples.UtxoAccumulator as S
 
 accumulationGroupElements :: Vector N BLS12_381_G1_Point
 accumulationGroupElements = unsafeToVector $ fromJust $ decode $ fromStrict $(embedFileRelative "accumulation-group-elements.json")
